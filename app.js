49c58789511b175a813e115a1d9ac6402d0c3db2
Oswaldo <oswaldojsotto@gmail.com>
2026-06-10 11:45:59 +0200

    security commit

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ import { logger } from "./middleware/logger.js";
 import { timing } from "./middleware/timing.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import userRoutes from "./routes/users.routes.js";
-import dotenv from "dotenv";
 
 const app = express();
 
 app.disable("x-powered-by");
 
 app.use(cors());
-app.use(dotenv.config());
 app.use(express.json());
 app.use(logger);
 app.use(timing);
